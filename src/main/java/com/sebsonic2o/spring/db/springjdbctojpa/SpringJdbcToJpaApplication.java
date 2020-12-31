1ab2276af799f8920bba11be00a90125698021dc
Sebastien Cheraux <scheraux@gmail.com>
2020-12-31 09:20:22 -0800

    find all with spring jdbc

--- src/main/java/com/sebsonic2o/spring/db/springjdbctojpa/SpringJdbcToJpaApplication.java
+++ src/main/java/com/sebsonic2o/spring/db/springjdbctojpa/SpringJdbcToJpaApplication.java
@@ -1,13 +1,28 @@
 package com.sebsonic2o.spring.db.springjdbctojpa;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.sebsonic2o.spring.db.springjdbctojpa.jdbc.PersonJdbcDao;
+
 @SpringBootApplication
-public class SpringJdbcToJpaApplication {
+public class SpringJdbcToJpaApplication implements CommandLineRunner {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	PersonJdbcDao dao;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJdbcToJpaApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("All persons - {}", dao.findAll());
+	}
 }
---
+++ src/main/java/com/sebsonic2o/spring/db/springjdbctojpa/jdbc/PersonJdbcDao.java
@@ -0,0 +1,22 @@
+package com.sebsonic2o.spring.db.springjdbctojpa.jdbc;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.sebsonic2o.spring.db.springjdbctojpa.entity.Person;
+
+@Repository
+public class PersonJdbcDao {
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	public List<Person> findAll() {
+		return jdbcTemplate.query("select * from person",
+				new BeanPropertyRowMapper<Person>(Person.class));
+	}
+}

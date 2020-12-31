b7855fb8aabfb3c6941b79883f4a395683aa0a11
Sebastien Cheraux <scheraux@gmail.com>
2020-12-30 17:09:09 -0800

    initialize maven project with jdbc, jpa, h2 and web

---
+++ src/main/java/com/sebsonic2o/spring/db/springjdbctojpa/SpringJdbcToJpaApplication.java
@@ -0,0 +1,13 @@
+package com.sebsonic2o.spring.db.springjdbctojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcToJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbcToJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sebsonic2o/spring/db/springjdbctojpa/SpringJdbcToJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebsonic2o.spring.db.springjdbctojpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJdbcToJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
